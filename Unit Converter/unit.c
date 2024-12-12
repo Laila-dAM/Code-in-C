c92d892f62390103c18ecaa8e895db840f5a6e49
Laila Mota <lailamota.238@gmail.com>
2024-12-12 19:32:21 -0300

    Converts units of length, mass and temperature

--- Unit Converter/unit.c
+++ Unit Converter/unit.c
@@ -28,3 +28,17 @@ void convertMass() {
     else
         printf("Invalid choice.\n");
 }
+void convertTemperature() {
+    double value;
+    int choice;
+    printf("Enter value to convert: ");
+    scanf("%lf", &value);
+    printf("Select conversion: 1. Celsius to Fahrenheit 2. Fahrenheit to Celsius\n");
+    scanf("%d", &choice);
+    if (choice == 1)
+        printf("%.2lf Celsius = %.2lf Fahrenheit\n", value, (value * 9 / 5) + 32);
+    else if (choice == 2)
+        printf("%.2lf Fahrenheit = %.2lf Celsius\n", value, (value - 32) * 5 / 9);
+    else
+        printf("Invalid choice.\n");
+}
