4015a52903ceb52fe672363492236141b95bad54
Laila Mota <lailamota.238@gmail.com>
2024-12-12 19:24:12 -0300

    Convert between meters and kilometers

--- Unit Converter/unit.c
+++ Unit Converter/unit.c
@@ -7,3 +7,10 @@ void convertLength() {
     scanf("%lf", &value);
     printf("Select conversion: 1. Meters to Kilometers 2. Kilometers to Meters\n");
     scanf("%d", &choice);
+  if (choice == 1)
+        printf("%.2lf meters = %.2lf kilometers\n", value, value / 1000);
+    else if (choice == 2)
+        printf("%.2lf kilometers = %.2lf meters\n", value, value * 1000);
+    else
+        printf("Invalid choice.\n");
+}
