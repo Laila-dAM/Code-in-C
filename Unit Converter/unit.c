e1471b3eaf4ce66470f193334852803c02906645
Laila Mota <lailamota.238@gmail.com>
2024-12-12 19:26:18 -0300

    Converts between grams and kilograms

--- Unit Converter/unit.c
+++ Unit Converter/unit.c
@@ -14,3 +14,17 @@ void convertLength() {
     else
         printf("Invalid choice.\n");
 }
+void convertMass() {
+    double value;
+    int choice;
+    printf("Enter value to convert: ");
+    scanf("%lf", &value);
+    printf("Select conversion: 1. Grams to Kilograms 2. Kilograms to Grams\n");
+    scanf("%d", &choice);
+    if (choice == 1)
+        printf("%.2lf grams = %.2lf kilograms\n", value, value / 1000);
+    else if (choice == 2)
+        printf("%.2lf kilograms = %.2lf grams\n", value, value * 1000);
+    else
+        printf("Invalid choice.\n");
+}
