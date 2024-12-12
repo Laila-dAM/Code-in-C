4b6730bb7eda4810c34f1084435b2ac2bc5b64dc
Laila Mota <lailamota.238@gmail.com>
2024-12-12 20:00:43 -0300

    Performs the multiplication of two matrices and prints the result

--- Matrix Calculator/Matrix.c
+++ Matrix Calculator/Matrix.c
@@ -15,3 +15,23 @@ void subtractMatrices(int m, int n, int A[m][n], int B[m][n], int result[m][n]) 
         }
     }
 }
+
+void multiplyMatrices(int m, int n, int p, int A[m][n], int B[n][p], int result[m][p]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int m, int n, int matrix[m][n]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
